9f1ca64b008da24a728ff9b49494c7700309dd80
albin <albin@LAPTOP-JDKMTNHV>
2021-12-14 01:43:18 +0100

    added listener

--- src/app/utilities/video-stream.utility.ts
+++ src/app/utilities/video-stream.utility.ts
@@ -1,4 +1,5 @@
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { skip } from 'rxjs';
 import { RpcService } from 'src/services/rpc.service';
 
 export interface Stream {
@@ -11,10 +12,14 @@ export class VideoStreamUtility {
   localStream: MediaStream;
   remoteStream: MediaStream;
 
+  isHost = false;
+
   constructor(
     private rpcService: RpcService,
     private firestore: AngularFirestore
-  ) { }
+  ) {
+    this.listenOnNewCall();
+  }
 
   async startCall(callId?: string): Promise<Stream> {
     if (this.isCallActive) {
@@ -30,7 +35,10 @@ export class VideoStreamUtility {
     this.initializeStreams();
 
     if (callId) await this.joinCall(callId);
-    else await this.makeCall();
+    else {
+      this.isHost = true;
+      await this.makeCall();
+    }
 
     this.isCallActive = true;
 
@@ -69,7 +77,7 @@ export class VideoStreamUtility {
 
     await callDoc.set({
       date: new Date(),
-      offer
+      offer,
     });
 
     callDoc.snapshotChanges().subscribe((snapshot) => {
@@ -123,11 +131,40 @@ export class VideoStreamUtility {
     offerCandidates.snapshotChanges().subscribe((snapshot) => {
       snapshot.forEach((item) => {
         if (item.type === 'added') {
-          debugger
+          debugger;
           const data = item.payload.doc.data();
           this.rpcService.pc.addIceCandidate(new RTCIceCandidate(data));
         }
       });
     });
+  }
+
+  private listenOnNewCall(): void {
+    const callDoc = this.firestore.collection('calls');
+
+    callDoc
+      .snapshotChanges()
+      .pipe(skip(1))
+      .subscribe((res) => {
+        let latestItem: any;
+        let latestItemId: string;
+        res.map((item, index) => {
+          const itemData: any = item.payload.doc.data();
+
+          if (!Object.keys(itemData).includes('offer') || !this.isHost) return;
+
+          if (
+            !latestItem ||
+            latestItem?.date.seconds * 1000 < itemData.date.seconds * 1000
+          ) {
+            latestItem = itemData;
+            latestItemId = item.payload.doc.id;
+          }
+        });
+
+        console.log('latest item', latestItem);
+        console.log('latest item id', latestItemId);
+        this.joinCall(latestItemId);
+      });
   }
 }
